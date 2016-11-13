338189c9a8ee2da14b7939144d6142e87398ac60
Alan Ehret <alan.4186@yahoo.com>
2016-11-13 14:08:45 -0500

    create ga class

---
+++ ga/ga_top.php
@@ -0,0 +1,42 @@
+<?php
+
+class ga {
+
+  var $population;
+  var $nextPopulation;
+  
+  var $currentIndividual;
+int  var $meanClickRate;
+
+
+  function __construct($popSize) {
+    // init pop 
+  }
+
+  public function genNewPop() {
+    // cross over
+  }
+
+  private function computeCost() {
+
+  }
+
+  private function selectParents() {
+
+  }
+
+  private function crossoverParents() {
+
+  }
+
+  private function mutatePopulation() {
+
+  }
+
+  private function computeMeanClickRate() {
+
+  }
+
+  public function updateClickRate($genNumber, $popIndex, $success)
+}// end ga class
+?>
