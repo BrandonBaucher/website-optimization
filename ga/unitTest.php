baea67abc55d0f137d1eea750cd54cbfd7cb6b90
Brandon Baucher <brandon.baucher@gmail.com>
2016-11-16 17:39:14 -0500

    Add population unit tests

--- ga/unitTest.php
+++ ga/unitTest.php
@@ -1,6 +1,5 @@
 <?php
 
-include 'individual.php';
 include 'population.php';
 
 function printIndividual( $curr )
@@ -10,6 +9,17 @@ function printIndividual( $curr )
     print_r($genes);
     print("Stats:\n");
     print_r($curr->getStats());
+}
+
+function printPopulation( $curr )
+{
+    print("Size:\n");
+    $size = $curr->getSize();
+    print_r($size."\n");
+    print("First Individual:\n");
+    printIndividual($curr->individuals[0]);
+    print("Last Individual:\n");
+    printIndividual(end($curr->individuals));
 }
 
 print("Testing Individual class:\n");
@@ -26,5 +36,15 @@ printIndividual($test);
 print("Resetting Individual:\n");
 $test->resetIndividual(array(5,6,7));
 printIndividual($test);
+
+print("Testing Population class:\n");
+print("New Population Size 10:\n");
+$test2 = new Population(array(1,2,3,4,5),10);
+printPopulation($test2);
+
+print("Increment 1 success and 1 fail on first individual:\n");
+$test2->individuals[0]->increment(true);
+$test2->individuals[0]->increment(false);
+printPopulation($test2);
 
 ?>
