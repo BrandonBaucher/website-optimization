0777e8fe062b23dcaf08445f45d1b53d9946bc3d
Alan Ehret <alan.4186@yahoo.com>
2016-11-23 09:56:49 -0500

    add cost function

--- ga/ga_top.php
+++ ga/ga_top.php
@@ -2,12 +2,17 @@
 
 class ga {
 
+  int $popSize;
   var $population;
   var $nextPopulation;
   
-  var $currentIndividual;
-int  var $meanClickRate;
+  int $currentIndividual; // an index, not an individual class instance
 
+  // cost function variables
+  int $meanClickRate;
+  int $costSlope
+  
+  float $mutationRate = 0.01;
 
   function __construct($popSize) {
     // init pop 
@@ -17,26 +22,69 @@ int  var $meanClickRate;
     // cross over
   }
 
-  private function computeCost() {
+  private function computeCost() 
+  {
+    var $stats;
+    var $individualClickRate;
+    var $cost;
+
+    for ($i=0; $i<$this->popSize; $i++) { 
+      $stats = $this->population.getIndividual($i).getStats();
+      $individualClickRate = floatval(stats[0])/floatval(stats[1]);
+      // x = $individualClickRate - $this->meanClickRate
+      // m = costSlope
+      // b = 0.5, the midpoint
+   
+      $cost =(($individualClickRate - $this->meanClickRate) * $this->costSlope) + 0.5;
+      if($cost > 1.0) 
+      {
+        return 1.0;
+      }
+      elseif ($cost < 1.0) 
+      {
+        return 0.0;
+      }
+      else 
+      {
+        return $cost;
+      }  
+    } // End for
+  }
+
+  private function selectParents() 
+  {
 
   }
 
-  private function selectParents() {
+  private function crossoverParents() 
+  {
 
   }
 
-  private function crossoverParents() {
+  private function mutatePopulation() 
+  {
+    if (mt_rand() / mt_getrandmax() < $this->mutationRate) 
+    {
+      // choose random population member
+      int $mutantIdx = mt_rand(0,$this->popSize-1);
+      
+      // get gene not in current individual
+
+      
+
+      // reset individual
+    }
+  }
+
+  private function computeMeanClickRate() 
+  {
 
   }
 
-  private function mutatePopulation() {
+  public function updateClickRate($genNumber, $popIndex, $success) 
+  {
 
   }
 
-  private function computeMeanClickRate() {
-
-  }
-
-  public function updateClickRate($genNumber, $popIndex, $success)
 }// end ga class
 ?>
