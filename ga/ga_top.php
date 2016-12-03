7f29f054dcee275df99c4a7dc88dd4488da525a5
Brandon Baucher <brandon.baucher@gmail.com>
2016-12-02 23:23:25 -0500

    Fix syntax errors
    I'm so sorry... Blame Alan

--- ga/ga_top.php
+++ ga/ga_top.php
@@ -1,21 +1,24 @@
 <?php
 
+include 'population.php';
+
 class ga {
 
   var $popSize;
   var $population;
   var $nextPopulation;
-  var $availablePosts; 
+  var $availablePosts;
   var $currentIndividual; // an index, not an individual class instance
 
   // cost function variables
   var $meanClickRate;
-  var $costSlope
+  var $costSlope;
 
-  float $mutationRate = 0.01;
+  var $mutationRate = 0.01;
 
-  // init pop 
-  function __construct($popSize{ 
+  // init pop
+  function __construct($popSize){
+    $this->popSize = $popSize;
     $this->updateAvailablePosts();
     $this->population = new Population($this->availablePosts, $popSize);
     $this->nextPopulation = $this->population; // quickly init next pop with junk
@@ -25,8 +28,8 @@ class ga {
   public function genNewPop() {
 
     // select two parents
-    var $parent0 = $this.selectPrent();
-    var $parent1 = $this.selectPrent();
+    $parent0 = $this.selectPrent();
+    $parent1 = $this.selectPrent();
 
     // cross over
     $this->crossover($parent0, $parent1);
@@ -35,49 +38,49 @@ class ga {
     $this->mutatePopulation();
   }
 
-  private function computeCost() 
+  private function computeCost()
   {$this->crossover($parent0, $parent1);
-    var $stats;
-    var $individualClickRate;
-    var $cost;
+    $stats;
+    $individualClickRate;
+    $cost;
 
-    for ($i=0; $i<$this->popSize; $i++) { 
-      $stats = $this->population->getIndividual($i).getStats();
+    for ($i=0; $i<$this->popSize; $i++) {
+      $stats = $this->population->individuals[$i]->getStats();
       $individualClickRate = floatval(stats[0])/floatval(stats[1]);
       // x = $individualClickRate - $this->meanClickRate
       // m = costSlope
       // b = 0.5, the midpoint
 
       $cost =(($individualClickRate - $this->meanClickRate) * $this->costSlope) + 0.5;
-      if($cost > 1.0) 
+      if($cost > 1.0)
       {
         return 1.0;
       }
-      elseif ($cost < 1.0) 
+      elseif ($cost < 1.0)
       {
         return 0.0;
       }
-      else 
+      else
       {
         return $cost;
-      }  
+      }
     } // End for
   }
 
-  private function selectParent() 
+  private function selectParent()
   {
     // roulet wheel selection for a single parent, will be called twice for crossover
     // returns the index of the selected parent
 
-    var $wheel = array();
-    var $costSum = 0;
-    var $upperBound;
-    var $parentIndex;
+    $wheel = array();
+    $costSum = 0;
+    $upperBound;
+    $parentIndex;
 
-    // build 'roulet wheel' by adding fitnesses 
+    // build 'roulet wheel' by adding fitnesses
     for($i=0; $i<$this->popSize; $i++) {
       $wheel[$i] = costSum + $this->population->individuals[$i]->fitness;
-      $costSum = costSum + $this->population->individuals[$i]->fitness; 
+      $costSum = costSum + $this->population->individuals[$i]->fitness;
     } // end for
 
     // randomly select povar on wheel
@@ -93,20 +96,20 @@ class ga {
     return $parentIndex;
   }
 
-  private function singleCrossover($parent0, $parent1) 
+  private function singleCrossover($parent0, $parent1)
   {
-    var $numGenes = count($parent0->getGenes());
-    // concat parents 
-    var $geneSet = array_unique(array_merge($parent0, $parent1));
+    $numGenes = count($parent0->getGenes());
+    // concat parents
+    $geneSet = array_unique(array_merge($parent0, $parent1));
     // pull 3 random genes
-    var $newGenes = array_rand($geneSet, $numGenes);
+    $newGenes = array_rand($geneSet, $numGenes);
     // return new genes
-    return $newGenes
-  } 
+    return $newGenes;
+  }
 
-  private function crossover($parent0, $parent1) 
+  private function crossover($parent0, $parent1)
   {
-    var $newGenes;
+    $newGenes;
 
     for($i=0; $i<$this->popSize; $i=$i+1)
     {
@@ -115,30 +118,30 @@ class ga {
     } // end for
   }
 
-  private function mutatePopulation() 
+  private function mutatePopulation()
   {
-    if (mt_rand() / mt_getrandmax() < $this->mutationRate) 
+    if (mt_rand() / mt_getrandmax() < $this->mutationRate)
     {
       // choose random population member
-      var $mutantIdx = mt_rand(0,$this->popSize-1);
-      var $mutantIndividual = $this->population[$mutantIdx];
+      $mutantIdx = mt_rand(0,$this->popSize-1);
+      $mutantIndividual = $this->population[$mutantIdx];
       // get gene not in current individual
-      // ASSUMING $availPosts IS JUST AN INDEXED ARRAY OF DB IDs  
-      var $newGene = mt_rand(0,count($this->availabePosts)-1); // just gets the index
+      // ASSUMING $availPosts IS JUST AN INDEXED ARRAY OF DB IDs
+      $newGene = mt_rand(0,count($this->availabePosts)-1); // just gets the index
       $newGene = $this->availablePosts[$newGene];
       // randomly choose mutant gene index
-      var $geneIdx = mt_rand(0,count($this->population[$mutantIdx]->getGenes()));
-      var $genes = $this->population[$mutantIdx]->getGenes();
+      $geneIdx = mt_rand(0,count($this->population[$mutantIdx]->getGenes()));
+      $genes = $this->population[$mutantIdx]->getGenes();
       $genes[$geneIdx] = $newGene;
-      $this->population[$mutantIdx]->resetIndvidual($genes);   
+      $this->population[$mutantIdx]->resetIndvidual($genes);
      } // end if
   }
 
-  private function computeMeanClickRate($genNumber) 
+  private function computeMeanClickRate($genNumber)
   {
     $success = 0;
     $total = 0;
-    for($i=0;i<count($this->population->individuals);$i++){
+    for($i=0;$i<count($this->population->individuals);$i++){
       $success += $this->population->individuals[$i]->getStats()[0];
       $total += $this->population->individuals[$i]->getStats()[1];
     }
@@ -151,8 +154,19 @@ class ga {
   }
 
   private function updateAvailablePosts() {
-    //    $this->availablePosts = /*MAGIC!*/;
-  } 
+    $this->availablePosts = array(1,2,3,4,5,6,7,8);/*MAGIC!*/;
+  }
+
+  public function unitTest()
+  {
+    for($i=0;$i<1000;$i++){
+      $this->updateClickRate(mt_rand(0,$this->popSize-1),mt_rand(0,1));
+    }
+    $this->computeMeanClickRate(0);
+    print("Mean click rate: ");
+    print($this->meanClickRate);
+    print("\n");
+  }
 
 }// end ga class
 ?>
