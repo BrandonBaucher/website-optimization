3dd705d6071bef177096cf0debd1f24e0a84da11
Brandon Baucher <brandon.baucher@gmail.com>
2016-12-10 15:13:55 -0500

    Fix click rate updating

--- ga/ga_verification.php
+++ ga/ga_verification.php
@@ -9,18 +9,26 @@ $fd = fopen ('out.csv', 'w');
 fclose($fd);
 $myGa = new ga(10);
 for($i=0;$i<$NUM_GEN;$i++){
-  for($popIndex=0;$popIndex<count($myGa->population);$popIndex++){
+  for($popIndex=0;$popIndex<count($myGa->population->individuals);$popIndex++){
     $genes = $myGa->population->individuals[$popIndex]->getGenes();
     $count = 0;
     for($j=0;$j<3;$j++){
       $count += ($genes[$j]%$SUCCESS_VAL==0)? 1 : 0;
     }
 
-    for($j=0;$j<1000;$j++){
-      $myGa->updateClickRate($popIndex, (mt_rand(0,1)<.2*($count+1)));
+    for($j=0;$j<2000*($count+1);$j++){
+      //if(mt_rand(0,1) < .2*($count+1))//TODO why doesn't this work.......
+        //$bool = True;
+      //else
+        //$bool = False;
+      $myGa->updateClickRate($popIndex, True);
+    }
+    for($j=0;$j<10000-2000*($count+1);$j++){
+      $myGa->updateClickRate($popIndex, False);
     }
 
   }
+  print_r($myGa->population->individuals[0]);
   print($myGa->meanClickRate."\n");
   saveTimeStep( $myGa );
   $myGa->genNewPop();
