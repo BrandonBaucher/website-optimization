88f9da77f9865f1f093792bf4dace8ea5ab59d8b
Brandon Baucher <brandon.baucher@gmail.com>
2016-12-13 13:40:42 -0500

    Add wordpress and GA capabilities to JS

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
      $("#related").click(function(){
-        $.post("http://localhost:8080/dummy_backend.php",
+        $.post("http://localhost:8080/",
         {
-          related: "true"
+          'action': 'my_action',
+          'related': "true"
+          'individual': 0,//update code for Id
         },
         function(){
         });
         //alert("post sent");
     });
    $("#unrelated").click(function(){
-        $.post("http://localhost:8080/dummy_backend.php",
+        $.post("http://localhost:8080/",
         {
-          related: "false"
+          'action': 'my_action',
+          'related': "false"
+          'individual': 0,//update code for Id
         },
         function(){
         });
